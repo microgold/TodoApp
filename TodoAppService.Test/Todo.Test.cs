b6fd7156b98bc988b7188ef9e4b583f310494a4c
microgold <Batman134!!>
2025-11-07 17:13:16 -0600

    moved all files on the back end up to the backend folder

--- TodoAppService.Test/Todo.Test.cs
+++ TodoAppService.Test/Todo.Test.cs
@@ -1,9 +1,10 @@
 using Microsoft.EntityFrameworkCore;
-using TodoApp.Api.Controllers;
 using TodoApp.Api.Data;
 using TodoApp.Api.Models;
 using Microsoft.AspNetCore.Mvc;
-using Xunit;
+using TodoApp.Controllers;
+using TodoApp.Services;
+using System;
 
 namespace TodoApp.Tests
 {
@@ -15,7 +16,7 @@ namespace TodoApp.Tests
                 .UseInMemoryDatabase(Guid.NewGuid().ToString())
                 .Options;
             var context = new AppDbContext(options);
-            context.Tasks.Add(new TaskItem { Title = ""Sample task"" });
+            context.Tasks.Add(new TaskItem { Title = "Sample task" });
             context.SaveChanges();
             return context;
         }
@@ -24,7 +25,8 @@ namespace TodoApp.Tests
         public async Task GetAll_ReturnsOk()
         {
             var ctx = GetInMemoryContext();
-            var controller = new TasksController(ctx);
+            TaskService service = new TaskService(ctx);
+            var controller = new TasksController(service);
             var result = await controller.GetAll();
             Assert.IsType<OkObjectResult>(result);
         }
@@ -33,8 +35,9 @@ namespace TodoApp.Tests
         public async Task Create_AddsTask()
         {
             var ctx = GetInMemoryContext();
-            var controller = new TasksController(ctx);
-            var newTask = new TaskItem { Title = ""New Task"" };
+            TaskService service = new TaskService(ctx);
+            var controller = new TasksController(service);
+            var newTask = new TaskItem { Title = "New Task" };
             var result = await controller.Create(newTask);
             Assert.IsType<CreatedAtActionResult>(result);
             Assert.Equal(2, ctx.Tasks.Count());
