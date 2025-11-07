c21e9c46612ae0ac467a4eaf2c585a00f9b1aee9
microgold <Batman134!!>
2025-11-07 15:17:56 -0600

    also include tasksSlice.ts

--- frontend/src/features/tasks/tasksSlice.ts
+++ frontend/src/features/tasks/tasksSlice.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axiosClient from "../../api/axiosClient";
+import axiosClient from "../../api/axiosClient";    
 import { Task, TaskState } from "./taskTypes";
 
 // --------------------
